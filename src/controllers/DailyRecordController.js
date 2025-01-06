9c36ea46b430884deeaf810ca6e1d2e852b221fe
Lionel-Feuga <feuga.lionel1@gmail.com>
2025-01-06 11:12:19 +0100

    transaction added to DailyReconrdController.addOrUpdateDailyRecord

--- src/controllers/DailyRecordController.js
+++ src/controllers/DailyRecordController.js
@@ -14,27 +14,38 @@ exports.addOrUpdateDailyRecord = async (req, res) => {
   const { date, cigarettesSmoked } = req.body;
 
   try {
-    let record = await DailyRecord.findOne({
-      where: {
-        userId: req.user.id,
-        date,
-      },
-    });
-
-    if (record) {
-      record.cigarettesSmoked = cigarettesSmoked;
-      await record.save();
-      return res.json({ message: "Enregistrement mis à jour", record });
-    } else {
-      record = await DailyRecord.create({
-        userId: req.user.id,
-        date,
-        cigarettesSmoked,
+    await sequelize.transaction(async (transaction) => {
+      let record = await DailyRecord.findOne({
+        where: {
+          userId: req.user.id,
+          date,
+        },
+        transaction, 
       });
-      return res.status(201).json({ message: "Enregistrement créé", record });
-    }
+
+      if (record) {
+        record.cigarettesSmoked = cigarettesSmoked;
+        await record.save({ transaction }); 
+        return res.json({ message: "Enregistrement mis à jour", record });
+      } else {
+        record = await DailyRecord.create(
+          {
+            userId: req.user.id,
+            date,
+            cigarettesSmoked,
+          },
+          { transaction } 
+        );
+        return res.status(201).json({ message: "Enregistrement créé", record });
+      }
+    });
   } catch (error) {
-    console.error("Erreur lors de l'ajout ou de la mise à jour de l'enregistrement :", error);
-    res.status(500).json({ error: "Erreur lors de l'ajout ou de la mise à jour de l'enregistrement" });
+    console.error(
+      "Erreur lors de l'ajout ou de la mise à jour de l'enregistrement :",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur lors de l'ajout ou de la mise à jour de l'enregistrement" });
   }
 };
